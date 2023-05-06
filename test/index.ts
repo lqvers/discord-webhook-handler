361ecb4917cf49948dce3f4e77d25e44673f3d39
jackson <nervisnervous@gmail.com>
2023-05-06 00:11:51 -0400

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,6 @@
 import { Webhook } from '../src';
 const webhook = new Webhook(
-  'https://discord.com/api/webhooks/1104247982347534396/pQ0uBl2HmzOeHuG7j-MvfcpKYpZ0zUqajV8o1Yrnx45cFJ0qaNlnb7nd42XsgOLvtP8r',
-  { username: 'Test', avatar_url: 'https://techcrunch.com/wp-content/uploads/2010/10/pirate.jpg' },
+  'null'
 );
 
 webhook.send({
@@ -14,10 +13,3 @@ webhook.send({
     },
   ],
 });
-
-webhook.update({
-  name: 'BALLS',
-  avatar: 'https://shiz.tk/img/favicon.png',
-});
-
-webhook.fetch().then((res) => console.log(res));
