f4d1faebca9c85c0719ff5bbc8834562a79d2a58
jackson <nervisnervous@gmail.com>
2023-05-06 00:13:52 -0400

    what

--- README.md
+++
@@ -1,2 +0,0 @@
-# hookflow
- An easy-to-use package for sending, managing, and deleting Discord webhooks.
--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\Owner\\Documents\\Portfolio Github\\hookflow\\docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\Owner\\Documents\\Portfolio Github\\hookflow\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\Owner\\Documents\\Portfolio Github\\hookflow\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\Owner\\Documents\\Portfolio Github\\hookflow\\docs\\src\\css\\custom.css'),
-];
