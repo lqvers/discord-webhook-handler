19a5a97af03e9b50ed62b81eddfc905eb9f163d1
jackson <nervisnervous@gmail.com>
2023-05-06 00:12:57 -0400

    uhm

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\Owner\\Documents\\Portfolio Github\\hookflow\\docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\Owner\\Documents\\Portfolio Github\\hookflow\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\Owner\\Documents\\Portfolio Github\\hookflow\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\Owner\\Documents\\Portfolio Github\\hookflow\\docs\\src\\css\\custom.css'),
+];
